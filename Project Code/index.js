9e1d02f2dc72a540b48fd0a0aace79dbdb88cdd0
JOHNOTTERBEIN <joot9454@colorado.edu>
2022-11-10 12:23:16 -0700

    addded recommendation tab

--- Project Code/index.js
+++ Project Code/index.js
@@ -70,7 +70,7 @@ app.get('/recommendation', (req, res) => {
 app.post('/submit_books', (req, res) => {
     //ACTUALLY SUBMIT BOOKS HERE
     res.render('Pages/login');
-
+});
 //TODO: add input to user_to_book table based on session var
 //TODO: add error checking
 app.post('/submit_books', async (req, res) => {
